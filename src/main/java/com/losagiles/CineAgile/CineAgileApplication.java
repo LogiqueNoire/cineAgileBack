bac7bb283be96813925487e8d9825684f94472bc
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-04-22 20:48:27 -0500

    00

---
+++ src/main/java/com/losagiles/CineAgile/CineAgileApplication.java
@@ -0,0 +1,13 @@
+package com.losagiles.CineAgile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineAgileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/losagiles/CineAgile/CineAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losagiles.CineAgile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineAgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
