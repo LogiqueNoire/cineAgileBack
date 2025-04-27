6d633d56da2c1156895590b6e6b24904de7fe2c4
spiderdog22 <spiderdog22@gmail.com>
2025-04-27 18:14:21 -0500

    Agregar atributo "imageUrl"

--- src/main/java/com/losagiles/CineAgile/entidades/Pelicula.java
+++ src/main/java/com/losagiles/CineAgile/entidades/Pelicula.java
@@ -11,12 +11,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
 import java.time.LocalDate;
 
 /**
  *
  * @author aryel:v
  */
+@Data
 @Entity
 @Table (name = "Pelicula")
 public class Pelicula {
@@ -56,22 +59,8 @@ public class Pelicula {
     @Column (nullable = false)
     String estado;
 
-    public Pelicula() {}
-
-    public Pelicula(String nombre, int duracion, String sinopsis, 
-                    String genero, String director, String clasificacion, String actores, 
-                    LocalDate fechaInicioPreventa, LocalDate fechaInicioEstreno, String estado) {
-        this.nombre = nombre;
-        this.duracion = duracion;
-        this.sinopsis = sinopsis;
-        this.genero = genero;
-        this.director = director;
-        this.clasificacion = clasificacion;
-        this.actores = actores;
-        this.fechaInicioPreventa = fechaInicioPreventa;
-        this.fechaInicioEstreno = fechaInicioEstreno;
-        this.estado = estado;
-    }
+    @Column (nullable = false)
+    String imageUrl;
 
     @Override
     public String toString() {
@@ -90,91 +79,4 @@ public class Pelicula {
         return sb.toString();
     }
 
-    public long getIdPelicula() {
-        return idPelicula;
-    }
-
-    public void setIdPelicula(long idPelicula) {
-        this.idPelicula = idPelicula;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getDuracion() {
-        return duracion;
-    }
-
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
-    }
-
-    public String getSinopsis() {
-        return sinopsis;
-    }
-
-    public void setSinopsis(String sinopsis) {
-        this.sinopsis = sinopsis;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getClasificacion() {
-        return clasificacion;
-    }
-
-    public void setClasificacion(String clasificacion) {
-        this.clasificacion = clasificacion;
-    }
-
-    public String getActores() {
-        return actores;
-    }
-
-    public void setActores(String actores) {
-        this.actores = actores;
-    }
-
-    public LocalDate getFechaInicioPreventa() {
-        return fechaInicioPreventa;
-    }
-
-    public void setFechaInicioPreventa(LocalDate fechaInicioPreventa) {
-        this.fechaInicioPreventa = fechaInicioPreventa;
-    }
-
-    public LocalDate getFechaInicioEstreno() {
-        return fechaInicioEstreno;
-    }
-
-    public void setFechaInicioEstreno(LocalDate fechaInicioEstreno) {
-        this.fechaInicioEstreno = fechaInicioEstreno;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
 }
