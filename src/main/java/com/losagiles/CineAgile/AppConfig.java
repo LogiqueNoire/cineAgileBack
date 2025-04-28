6a0817ddcac3e99cc4782735f607b25ba67a7104
spiderdog22 <spiderdog22@gmail.com>
2025-04-27 21:23:11 -0500

    Permitir solicitudes desde el front local

--- src/main/java/com/losagiles/CineAgile/AppConfig.java
+++ src/main/java/com/losagiles/CineAgile/AppConfig.java
@@ -1,13 +1,32 @@
 package com.losagiles.CineAgile;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
+    }
+
+    @Profile("dev")
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**") // Apply to all endpoints
+                            .allowedOrigins("*")
+                            .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                            .allowedHeaders("*"); // Allowed headers
+                }
+            };
     }
 }
