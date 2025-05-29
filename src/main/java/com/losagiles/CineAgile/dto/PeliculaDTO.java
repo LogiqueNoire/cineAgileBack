67563b42a2120a1767c9884b181ce521286af3f5
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-05-28 20:50:40 -0500

    eliminar atributo fechaInicioEstreno dto

--- src/main/java/com/losagiles/CineAgile/dto/PeliculaDTO.java
+++ src/main/java/com/losagiles/CineAgile/dto/PeliculaDTO.java
@@ -13,11 +13,10 @@ public class PeliculaDTO {
     private Integer duracion;
     private String estado;
     private String fechaInicioEstreno;
-    private String fechaInicioPreventa;
     private String imageUrl;
     private String sinopsis;
 
-    public PeliculaDTO(String nombre, String director, String actores, String genero, String clasificacion, Integer duracion, String estado, String fechaInicioEstreno, String fechaInicioPreventa, String imageUrl, String sinopsis) {
+    public PeliculaDTO(String nombre, String director, String actores, String genero, String clasificacion, Integer duracion, String estado, String fechaInicioEstreno, String imageUrl, String sinopsis) {
 
         this.nombre = nombre;
         this.director = director;
@@ -27,7 +26,6 @@ public class PeliculaDTO {
         this.duracion = duracion;
         this.estado = estado;
         this.fechaInicioEstreno = fechaInicioEstreno;
-        this.fechaInicioPreventa = fechaInicioPreventa;
         this.imageUrl = imageUrl;
         this.sinopsis = sinopsis;
     }
