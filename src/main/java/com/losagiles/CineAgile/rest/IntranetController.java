e66830ab18f1698bc5c285f8eacd25f10e1a8519
spiderdog22 <spiderdog22@gmail.com>
2025-05-30 19:18:22 -0500

    Nueva subruta /intranet que retorna el nombre del usuario

--- src/main/java/com/losagiles/CineAgile/rest/IntranetController.java
+++ src/main/java/com/losagiles/CineAgile/rest/IntranetController.java
@@ -11,6 +11,11 @@ import com.losagiles.CineAgile.services.SalaButacasService;
 import com.losagiles.CineAgile.services.SedeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -86,5 +91,14 @@ public class IntranetController {
         return ResponseEntity.ok(guardada);
     }
 
+    @GetMapping("/user")
+    public ResponseEntity<String> getUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated())
+            return ResponseEntity.status(401).body("no autenticado");
+
+        return ResponseEntity.ok((String) auth.getPrincipal());
+    }
 
 }
