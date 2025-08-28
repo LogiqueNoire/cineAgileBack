7f0c473fdaa4b73e22fadda7f13cfe0128ecadd8
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-08-27 22:20:38 -0500

    feat: :technologist: poblar la BD con datos de prueba

--- src/main/java/com/losagiles/CineAgile/services/ScriptService.java
+++ src/main/java/com/losagiles/CineAgile/services/ScriptService.java
@@ -1,5 +1,53 @@
 package com.losagiles.CineAgile.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.List;
+
+@Service
+public class ScriptService {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private static final List<String> SCRIPTS_PERMITIDOS = List.of(
+            "generos_query.sql",
+            "sedes_salas_inserts_query.sql",
+            "peliculas_inserts_query.sql",
+            "butacas_query.sql"
+    );
+/*
+    public void ejecutarScript(String nombreScript) throws Exception {
+        if(!SCRIPTS_PERMITIDOS.contains(nombreScript)){
+            throw new IllegalArgumentException("Scrip no permitido");
+        }
+        try (Connection connection = dataSource.getConnection()){
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("/sql/"+nombreScript));
+        }
+    }
+*/
+    public void poblarBD() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            for(String s: SCRIPTS_PERMITIDOS) {
+                try {
+                    ScriptUtils.executeSqlScript(connection, new ClassPathResource("/dbscripts/" + s));
+                    System.out.println("Script ejecutado correctamente: " + s);
+                } catch (Exception e) {
+                    System.err.println("Error en script " + s + ": " + e.getMessage());
+                    throw e;
+                }
+            }
+        }
+    }
+}
+
+package com.losagiles.CineAgile.services;
+
 import com.losagiles.CineAgile.otros.Auditable;
 import com.losagiles.CineAgile.otros.TipoAccion;
 import org.springframework.beans.factory.annotation.Autowired;
