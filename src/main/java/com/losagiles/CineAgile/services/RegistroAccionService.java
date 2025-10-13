3004162db0436a16570507a464615a9c30a5a98e
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-10-13 13:29:13 -0500

    fix: @Transactional(propagation = Propagation.REQUIRES_NEW) en registrarAccion

--- src/main/java/com/losagiles/CineAgile/services/RegistroAccionService.java
+++ src/main/java/com/losagiles/CineAgile/services/RegistroAccionService.java
@@ -4,8 +4,10 @@ import com.losagiles.CineAgile.entidades.RegistroAccion;
 import com.losagiles.CineAgile.otros.Auditable;
 import com.losagiles.CineAgile.otros.TipoAccion;
 import com.losagiles.CineAgile.repository.RegistroAccionRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
@@ -16,12 +18,18 @@ public class RegistroAccionService {
     @Autowired
     private RegistroAccionRepository registroAccionRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public RegistroAccion registrarAccion(RegistroAccion accion) {
         return registroAccionRepository.saveAndFlush(accion);
     }
 
     @Auditable(value = TipoAccion.CONSULTAR, nombreEntidad = "Auditoría", detalles = "Consultar registros de auditoría")
+    @Transactional
     public List<RegistroAccion> listarAcciones() {
+        entityManager.clear();
         return registroAccionRepository.findAll();
     }
 }
